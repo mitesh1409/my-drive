0ca13ccebdf787874e649e4fabcc7f5f2abf4978
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-04 18:17:49 +0530

    feature: Add code to display app home page, refactor code to use controller

---
+++ controllers/static.controller.js
@@ -0,0 +1,7 @@
+function home(req, res) {
+    res.render('static/home');
+}
+
+export {
+    home,
+};
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import * as StaticController from './controllers/static.controller.js';
+
 const PORT = 3000;
 const HOSTNAME = '127.0.0.1';
 
@@ -7,8 +9,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.get('/', StaticController.home);
+app.get('/home', StaticController.home);
 
 app.listen(PORT, HOSTNAME, () => console.log(`Server up and running at http://${HOSTNAME}:${PORT}`));
