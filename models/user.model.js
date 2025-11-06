c750f73545212da99883943496017c26297f5aff
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-06 17:01:05 +0530

    feature: Add code to create user upon sign up request

---
+++ models/user.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+export {
+    User
+};
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import { User } from '../models/user.model.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/', (req, res) => {
+userRouter.post('/', async (req, res) => {
     console.log('Called POST /users');
     console.log('Request', req.body);
+
+    const { firstName, lastName, email, password } = req.body;
+
+    console.log(firstName, lastName, email, password);
+
+    await User.create({
+        firstName,
+        lastName,
+        email,
+        password
+    });
 
     res.send('User created successfully!');
 });
