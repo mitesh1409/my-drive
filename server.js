3881206727afe1aa0600437b16c56704f8649f84
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-06 16:15:18 +0530

    feature: Set common middlewares

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+// Set common middlewares.
+// Serves static assets such as HTML files, images, and so on from the public directory.
+app.use(express.static('public'));
+// Parses incoming requests with JSON payloads.
+app.use(express.json());
+// Parses incoming requests with URL-encoded payloads, this is required to handle form request data.
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', homeController);
 app.get('/home', homeController);
 app.get('/sign-up', signupController);
