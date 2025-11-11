b3e4505748b39cb216a51f3130af2348d0830099
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-11 15:49:44 +0530

    feature: Add validations in the user schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,47 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
+    firstName: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+        minLength: {
+            value: 3,
+            message: 'First name must be at least 3 characters long'
+        }
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+        minLength: {
+            value: 3,
+            message: 'Last name must be at least 3 characters long'
+        }
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+        minLength: {
+            value: 8,
+            message: 'Password must be minimum 8 characters long'
+        },
+        maxLength: {
+            value: 20,
+            message: 'Password must be maximum 20 characters long'
+        }
+    },
 });
 
 const User = mongoose.model('User', userSchema);
