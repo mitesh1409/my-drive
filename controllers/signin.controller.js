3622235793011242ba365fad7298f9a24733e5a2
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-11 18:09:45 +0530

    feature: Login API development changes - WIP

--- controllers/signin.controller.js
+++ controllers/signin.controller.js
@@ -1,7 +1,60 @@
-function signinController(req, res) {
+import { validationResult } from "express-validator";
+import bcrypt from 'bcrypt';
+import { User } from "../models/user.model";
+
+function displaySignin(req, res) {
     res.render('sign-in');
 }
 
+async function doSignin(req, res) {
+    const validationResults = validationResult(req);
+    if (!validationResults.isEmpty()) {
+        res
+            .status(400)
+            .json({
+                status: 'Bad Request',
+                message: 'Failed to login',
+                errors: validationResults.array()
+            });
+        return;
+    }
+
+    const { email, password } = req.body;
+
+    // Get user by email.
+    const user = await User.findOne({email: email});
+
+    if (!user) {
+        res
+            .status(400)
+            .json({
+                status: 'Bad Request',
+                message: 'Failed to login',
+                errors: ['Email or Password is incorrect']
+            });
+        return;
+    }
+
+    // Check password.
+    const passwordMatched = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatched) {
+        res
+            .status(400)
+            .json({
+                status: 'Bad Request',
+                message: 'Failed to login',
+                errors: ['Email or Password is incorrect']
+            });
+        return;
+    }
+
+    // All good. Now we can proceed with generating auth token for the user.
+
+
+}
+
 export {
-    signinController
+    displaySignin,
+    doSignin
 };
--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import 'dotenv/config'; // Loads environment variables from .env file.
 
 import { homeController } from './controllers/home.controller.js';
 import { signupController } from './controllers/signup.controller.js';
-import { signinController } from './controllers/signin.controller.js';
+import * as SigninController from './controllers/signin.controller.js';
 import { userRouter } from './routes/user.router.js';
 import { requestLogger } from './middlewares/requestLogger.js';
 import { connectDB } from './config/db.js';
+import { body } from 'express-validator';
 
 const PORT = 3000;
 const HOSTNAME = '127.0.0.1';
@@ -30,7 +31,19 @@ app.use(express.urlencoded({extended: true}));
 app.get('/', homeController);
 app.get('/home', homeController);
 app.get('/sign-up', signupController);
-app.get('/sign-in', signinController);
+app.get('/sign-in', SigninController.displaySignin);
+app.post(
+    '/sign-in',
+    body('email')
+        .trim()
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email is invalid'),
+    body('password')
+        .trim()
+        .notEmpty().withMessage('Password is required')
+        .isLength({min: 8, max: 20}).withMessage('Password must be 8 to 20 characters long'),
+    SigninController.doSignin
+);
 
 app.use('/users', userRouter);
 
