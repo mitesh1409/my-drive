f852d1f7ef6a39548700a335fa17ff47d6460586
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-01-06 13:52:41 +0530

    🚀

--- config/multer.config.js
+++ config/multer.config.js
@@ -3,11 +3,11 @@ import FirebaseStorage from "multer-firebase-storage";
 import { cert } from "firebase-admin/app";
 import serviceAccount from "../my-drive-735c7-firebase-adminsdk-fbsvc-608ba53203.json" with { type: "json" };
 
-const multer = Multer({
+const upload = Multer({
   storage: FirebaseStorage({
     bucketName: 'my-drive-735c7.firebasestorage.app',
     credentials: cert(serviceAccount),
   })
 });
 
-export { multer };
+export { upload };
