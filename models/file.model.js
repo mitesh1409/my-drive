816ada9dfde4643c70ffa4fad4fa4655b2d10400
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-01-06 15:56:40 +0530

    🚀

---
+++ models/file.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema({
+    path: {
+        type: String,
+        trim: true,
+        required: [ true, 'File path is required' ],
+    },
+    originalName: {
+        type: String,
+        trim: true,
+        required: [ true, 'Original file name is required' ],
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: [ true, 'User is required' ],
+    }
+});
+
+const File = mongoose.model('files', fileSchema);
+
+export default File;
